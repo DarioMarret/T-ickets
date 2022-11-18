7dbc9ac7377c5875d2bff14e64bb2a4dd66abaa9
geovannysan <geovanny1santiago@gmail.com>
2022-11-18 17:43:07 -0500

    tabla

--- src/views/Pages/Susbcritorpage/TableFunc.js
+++ src/views/Pages/Susbcritorpage/TableFunc.js
@@ -17,9 +17,16 @@ export default function TableWiev(props) {
                 dataType: "json",
                 "language": {
                     "url": "//cdn.datatables.net/plug-ins/1.10.16/i18n/Spanish.json",
-
+                    "decimal": ",",
+                    "thousands": ".",
+                    "lengthMenu": "Mostrar _MENU_ registros",
+                    "zeroRecords": "No se encontraron resultados",
+                    "info": " Registros del _START_ al _END_ de un total de _TOTAL_ registros",
+                    "infoEmpty": "Registros del 0 al 0 de un total de 0 registros",
+                    "infoFiltered": "(filtrado de un total de _MAX_ registros)",
+                    "sSearch": "Buscar:",
                 },
-                "info": false,
+
                 responsive: true,
                 columns: [
                     { "data": "username", "title": "Username", },
@@ -29,12 +36,13 @@ export default function TableWiev(props) {
                     { "data": "name", "title": "Nombre", },
                     { "data": "perfil", "title": "Perfil", },
                     { "data": "password", "visible": false, },
-                    { "data": "action", "title": "acciones" }
+                    { "data": "action", "title": "acciones", "className": "col-12" }
                 ],
                 "columnDefs": [
                     { responsivePriority: 1, targets: 0 },
                     { responsivePriority: 2, targets: 4 }
                 ],
+                "dom": '<"row justify-content-between top-information"lf>rt<"row justify-content-between bottom-information"ip><"clear">',
                 destroy: true,
             }
         )
@@ -45,7 +53,7 @@ export default function TableWiev(props) {
     return (
         <div>
             <div className="col-12">
-                <table className=" responsive display nowrap " width="100%" id="table1" ref={tableRef}>
+                <table className=" responsive display nowrap table  table-bordered" width="100%" id="table1" ref={tableRef}>
                 </table>
             </div>
 
