0ba2bfd9830883ff1c5545abf656f26b8c31b4b1
geovannysan <geovanny1santiago@gmail.com>
2023-02-01 17:36:34 -0500

    datos push

--- src/views/Components/MODAL/ModalCarritov.js
+++ src/views/Components/MODAL/ModalCarritov.js
@@ -178,7 +178,7 @@ const ModalCarritoView = (prop) => {
             }))
             return
         }
-        if (clienteInfo() == null && CODIGO == "9EGM42" && (new Date("02/01/2023 19:10") < new Date())) {
+        if (clienteInfo() == null && CODIGO == "9EGM42" && (new Date("02/01/2023 23:59") < new Date())) {
             usedispatch(setToastes({
                 show: true,
                 message: "Estaremos informando la fecha de canje de boletos",
