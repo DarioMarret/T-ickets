7494e5411bb664f92808a60e700ebff3f77e6749
geovannysan <geovanny1santiago@gmail.com>
2023-02-01 22:29:14 -0500

    dos de febrero

--- src/views/Components/MODAL/ModalCarritov.js
+++ src/views/Components/MODAL/ModalCarritov.js
@@ -169,7 +169,7 @@ const ModalCarritoView = (prop) => {
         // console.log((sleccionlocalidad.pagados > 10))
         //console.log(sleccionlocalidad)
         //if (clienteInfo() == null && CODIGO == "9EGM42") {
-        if (clienteInfo() == null && CODIGO == "ANNKV7" && (new Date("02/01/2023 23:59") < new Date())) {
+        if (clienteInfo() == null && CODIGO == "ANNKV7" && (new Date("02/02/2023 08:30") < new Date())) {
             usedispatch(setToastes({
                 show: true,
                 message: "Estaremos informando la fecha de canje de boletos",
@@ -178,7 +178,7 @@ const ModalCarritoView = (prop) => {
             }))
             return
         }
-        if (clienteInfo() == null && CODIGO == "9EGM42" && (new Date("02/01/2023 23:59") < new Date())) {
+        if (clienteInfo() == null && CODIGO == "9EGM42" && (new Date("02/02/2023 08:30") < new Date())) {
             usedispatch(setToastes({
                 show: true,
                 message: "Estaremos informando la fecha de canje de boletos",
@@ -305,7 +305,7 @@ const ModalCarritoView = (prop) => {
             // console.log(sleccionlocalidad)
             //console.log(clienteInfo())
             let consulta = precios.precios.find((F) => F.idcolor == this.classList[0])
-            if (clienteInfo() == null && CODIGO == "ANNKV7" && (new Date("02/01/2023 23:59") < new Date())) {
+            if (clienteInfo() == null && CODIGO == "ANNKV7" && (new Date("02/02/2023 08:30") < new Date())) {
                 usedispatch(setToastes({
                     show: true,
                     message: "Estaremos informando la fecha de canje de boletos",
@@ -314,7 +314,7 @@ const ModalCarritoView = (prop) => {
                 }))
                 return
             }
-            if (clienteInfo() == null && CODIGO == "9EGM42" && (new Date("02/01/2023 23:59") < new Date())) {
+            if (clienteInfo() == null && CODIGO == "9EGM42" && (new Date("02/02/2023 08:30") < new Date())) {
                 usedispatch(setToastes({
                     show: true,
                     message: "Estaremos informando la fecha de canje de boletos",
